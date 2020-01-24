130000af760a3a0886e6c6b11c1f3568dd2a6941
Jo IE <joie.software@gmail.com>
2020-01-24 15:50:18 +0100

    Change port for Heroku

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ app.get('/:messageid', getMessage);
 app.delete('/delete/:messageid', deleteMessage);
 
 //listen for requests
-app.listen(3000, () => console.log('Sever is now listening on port 3000'))
+app.listen(process.env.PORT || 5000, () => console.log('Sever is now listening on port 5000'))
 
 module.exports=app;
